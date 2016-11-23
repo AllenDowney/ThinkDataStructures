8e9afeda195581e2f83230b555f44c56318de4e7
David Abrahams <davidabrahams@sbcglobal.net>
2016-11-22 23:22:31 -0800

    adding graph file

---
+++ graphs/src/Graph.java
@@ -0,0 +1,38 @@
+import java.util.List;
+
+public class Graph {
+
+    public Graph() {
+
+    }
+
+    public void addVertex(int v) {
+
+    }
+
+    public void addEdge(int v, int w) {
+
+    }
+
+    public List<Integer> vertices(int v) {
+        return null;
+    }
+
+    public int numVertices() {
+        return -1;
+    }
+
+    public int numEdges() {
+        return -1;
+    }
+
+    public Iterable<Integer> getNeighbors(int v) {
+        return null;
+    }
+
+    public boolean hasEdgeBetween(int v, int w) {
+        return false;
+    }
+
+
+}
