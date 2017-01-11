9de0c85e16f96619471fc4c40d461cffceec2a4c
David Abrahams <davidabrahams@sbcglobal.net>
2017-01-10 18:31:03 -0800

    removing unused imports

--- graphs/day00/src/GraphTest.java
+++ graphs/day00/src/GraphTest.java
@@ -2,8 +2,6 @@
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.*;
 
-import java.util.Collection;
-
 import org.junit.Before;
 import org.junit.Test;
 
--- graphs/day01/src/DFS.java
+++ graphs/day01/src/DFS.java
@@ -1,4 +1,3 @@
-import java.util.*;
 
 public class DFS {
 
