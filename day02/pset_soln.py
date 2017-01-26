cbc4e2b3de9cfcc0164c89e552b91c6ecd1433ad
David Abrahams <davidabrahams@sbcglobal.net>
2017-01-25 19:12:03 -0500

    data. not item

--- day02/pset_soln.py
+++ day02/pset_soln.py
@@ -32,7 +32,7 @@ def min_pairs_faster(arr):
 def equal(head1, head2):
 
     while (head1 is not None) and (head2 is not None):
-        if head1.item != head2.item:
+        if head1.data != head2.data:
             return False
         head1 = head1.next
         head2 = head2.next
