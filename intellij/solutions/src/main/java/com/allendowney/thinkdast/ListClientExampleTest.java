7b003eb2fb8d144b8f494fe87ca4924a078e8680
Dong Hwan Yu <koreacio@gmail.com>
2018-04-28 22:21:32 +0900

    [intellij] added ch01

---
+++ intellij/solutions/src/main/java/com/allendowney/thinkdast/ListClientExampleTest.java
@@ -0,0 +1,28 @@
+package com.allendowney.thinkdast;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+/**
+ * @author downey
+ *
+ */
+public class ListClientExampleTest {
+
+	/**
+	 * Test method for {@link ListClientExample}.
+	 */
+	@Test
+	public void testListClientExample() {
+		ListClientExample lce = new ListClientExample();
+		@SuppressWarnings("rawtypes")
+		List list = lce.getList();
+		assertThat(list, instanceOf(ArrayList.class) );
+	}
+
+}
